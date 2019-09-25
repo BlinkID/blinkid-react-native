f0fb2cd009d9433490d0ccc05afcee895a5670fc
Ivan Grce <ivan.grce@microblink.com>
2019-09-25 14:03:27 +0200

    Updated wrapper version to v4.11.2

--- initReactNativeDemoApp.sh
+++ initReactNativeDemoApp.sh
@@ -22,7 +22,7 @@ else
   # use directly source code from this repo instead of npm package
   # from RN 0.57 symlink does not work any more
   npm pack $blink_id_plugin_path
-  npm i --save blinkid-react-native-4.11.0.tgz
+  npm i --save blinkid-react-native-4.11.2.tgz
   npm install
   #pushd node_modules
     #ln -s $blink_id_plugin_path blinkid-react-native
