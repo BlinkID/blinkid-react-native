8c38a0a40790878276d48e4c2ef227982137e9d8
Jura Skrlec <jura.skrlec@microblink.com>
2020-02-06 11:17:52 +0100

    [ios] updated bindings v5.2.0

--- BlinkID/src/ios/MicroblinkModule/MicroblinkModule/Recognizers/Wrappers/MBBlinkIdCombinedRecognizerWrapper.m
+++ BlinkID/src/ios/MicroblinkModule/MicroblinkModule/Recognizers/Wrappers/MBBlinkIdCombinedRecognizerWrapper.m
@@ -52,6 +52,12 @@
             recognizer.returnFullDocumentImage = [(NSNumber *)returnFullDocumentImage boolValue];
         }
     }
+    {
+        id signResult = [jsonRecognizer valueForKey:@"signResult"];
+        if (signResult != nil) {
+            recognizer.signResult = [(NSNumber *)signResult boolValue];
+        }
+    }
 
     return recognizer;
 }
@@ -72,6 +78,8 @@
     [jsonResult setValue:[MBSerializationUtils serializeMBDateResult:self.result.dateOfBirth] forKey:@"dateOfBirth"];
     [jsonResult setValue:[MBSerializationUtils serializeMBDateResult:self.result.dateOfExpiry] forKey:@"dateOfExpiry"];
     [jsonResult setValue:[MBSerializationUtils serializeMBDateResult:self.result.dateOfIssue] forKey:@"dateOfIssue"];
+    [jsonResult setValue:[self.result.digitalSignature base64EncodedStringWithOptions:0] forKey:@"digitalSignature"];
+    [jsonResult setValue:[NSNumber numberWithUnsignedInteger:self.result.digitalSignatureVersion] forKey:@"digitalSignatureVersion"];
     [jsonResult setValue:self.result.documentAdditionalNumber forKey:@"documentAdditionalNumber"];
     [jsonResult setValue:[NSNumber numberWithInteger:(self.result.documentDataMatch + 1)] forKey:@"documentDataMatch"];
     [jsonResult setValue:self.result.documentNumber forKey:@"documentNumber"];
@@ -84,6 +92,7 @@
     [jsonResult setValue:self.result.fullName forKey:@"fullName"];
     [jsonResult setValue:self.result.issuingAuthority forKey:@"issuingAuthority"];
     [jsonResult setValue:self.result.lastName forKey:@"lastName"];
+    [jsonResult setValue:self.result.localizedName forKey:@"localizedName"];
     [jsonResult setValue:self.result.maritalStatus forKey:@"maritalStatus"];
     [jsonResult setValue:[MBBlinkIDSerializationUtils serializeMrzResult:self.result.mrzResult] forKey:@"mrzResult"];
     [jsonResult setValue:self.result.nationality forKey:@"nationality"];
