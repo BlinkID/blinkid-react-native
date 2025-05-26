5dec9cbff73747e2021764c264255d528121161b
Milan Paradina <milan.paradina@microblink.com>
2025-05-26 17:12:19 +0200

    fix android issue for data transfer

--- BlinkID/android/src/main/java/com/blinkidreactnative/BlinkidReactNativeModule.kt
+++ BlinkID/android/src/main/java/com/blinkidreactnative/BlinkidReactNativeModule.kt
@@ -2,11 +2,11 @@ package com.blinkidreactnative
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Bundle
 import com.facebook.react.bridge.BaseActivityEventListener
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
+import com.microblink.blinkid.ux.contract.BlinkIdScanActivityResultStatus
 import com.microblink.blinkid.ux.contract.BlinkIdScanActivitySettings
 import com.microblink.blinkid.ux.contract.MbBlinkIdScan
 import com.microblink.core.LicenseLockedException
@@ -15,6 +15,9 @@ import org.json.JSONObject
 @ReactModule(name = BlinkidReactNativeModule.NAME)
 class BlinkidReactNativeModule(reactContext: ReactApplicationContext) :
   NativeBlinkidReactNativeSpec(reactContext) {
+  private val BLINKID_REQUEST_CODE = 1453
+  private val BLINKID_ERROR_RESULT_CODE = "BlinkIdAndroid"
+
   var pendingPromise: Promise? = null
 
   override fun getName(): String {
@@ -24,15 +27,32 @@ class BlinkidReactNativeModule(reactContext: ReactApplicationContext) :
   init {
     reactApplicationContext.addActivityEventListener(object : BaseActivityEventListener() {
       override fun onActivityResult(activity: Activity?, requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 1040 && resultCode == Activity.RESULT_OK && data != null) {
-          val result = MbBlinkIdScan().parseResult(resultCode, data)
-          val resultJson = BlinkIdSerializationUtils.serializeBlinkIdScanningResult(result.result)
-          println("RESULTS: $resultJson")
-          pendingPromise?.resolve(resultJson.toString())
-        } else if (requestCode == 1040) {
-          pendingPromise?.reject("BlinkID", "Scan cancelled or failed.")
+        if (requestCode == BLINKID_REQUEST_CODE) {
+
+          val blinkIdResult = MbBlinkIdScan().parseResult(resultCode, data)
+          when (blinkIdResult.status) {
+            BlinkIdScanActivityResultStatus.DocumentScanned -> {
+              blinkIdResult.result?.let { scanningResult ->
+                val success = BlinkIdSerializationUtils.serializeBlinkIdScanningResult(
+                  scanningResult
+                )
+                pendingPromise?.resolve(success)
+
+              } ?: pendingPromise?.reject(BLINKID_ERROR_RESULT_CODE, "BlinkID result is empty.")
+            }
+
+            BlinkIdScanActivityResultStatus.Canceled -> {
+              pendingPromise?.reject(BLINKID_ERROR_RESULT_CODE, "Scanning is canceled.")
+            }
+
+            BlinkIdScanActivityResultStatus.ErrorSdkInit -> {
+              pendingPromise?.reject(
+                BLINKID_ERROR_RESULT_CODE,
+                "Could not initialize the SDK."
+              )
+            }
+          }
         }
-        pendingPromise = null
       }
     })
   }
@@ -54,7 +74,7 @@ class BlinkidReactNativeModule(reactContext: ReactApplicationContext) :
         val sdkSettings = sdkSettingsJson?.let {
           BlinkIdDeserializationUtils.deserializeBlinkIdSdkSettings(it)
         } ?: run {
-          pendingPromise?.reject("BlinkIdAndroid", "Invalid SDK settings.")
+          pendingPromise?.reject(BLINKID_ERROR_RESULT_CODE, "Invalid SDK settings.")
           return
         }
 
@@ -67,17 +87,16 @@ class BlinkidReactNativeModule(reactContext: ReactApplicationContext) :
               uxSettings = BlinkIdDeserializationUtils.deserializeBlinkIdUxSettings(sessionSettingsJson, classFilterJson)
             )
           )
-          currentActivity?.startActivityForResult(intent, 1040, Bundle())
-        }?: pendingPromise?.reject("BlinkIdAndroid", "Activity not found.")
+          currentActivity?.startActivityForResult(intent, BLINKID_REQUEST_CODE, null)
+        }?: pendingPromise?.reject(BLINKID_ERROR_RESULT_CODE, "Activity not found.")
       } catch (error: Exception) {
         when (error) {
           is LicenseLockedException -> {
-            pendingPromise?.reject("BlinkIdAndroid", error.message)
+            pendingPromise?.reject(BLINKID_ERROR_RESULT_CODE, error.message)
           }
 
           else -> {
-            println("SDK ERROR" + error)
-            pendingPromise?.reject("BlinkIdAndroid", error.message)
+            pendingPromise?.reject(BLINKID_ERROR_RESULT_CODE, error.message)
           }
         }
       }
