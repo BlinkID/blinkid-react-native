e0e03268ae15f261686bba56617a9bf0dfaf1f72
Milan Paradina <milan.paradina@microblink.com>
2025-06-04 12:42:34 +0200

    update gitignore

--- BlinkID/lib/typescript/src/blinkIdUtilities.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class BlinkIdUtilities {
-    /**
-     * Helper method for handling different string types,
-     * based on the document information source.
-     */
-    static handleStringType<T>(raw: any): T | undefined;
-}
-//# sourceMappingURL=blinkIdUtilities.d.ts.map
