cc478a2aeb39c4c95ebee55fbec14ee4e32812ed
mparadina <milan.paradina@microblink.com>
2025-12-09 14:03:14 +0100

    update sdk version

--- initBlinkIdReactNativeSample.sh
+++ initBlinkIdReactNativeSample.sh
@@ -3,7 +3,7 @@
 blink_id_plugin_path=`pwd`/BlinkID
 appName=BlinkIdSample
 appId=com.microblink.sample
-rn_version="0.82.0"
+rn_version="0.78.3"
 
 # Remove any existing code
 rm -rf $appName
@@ -23,7 +23,7 @@ sed -i '' '/"compilerOptions": {/a\
 \    "skipLibCheck": true,
 ' tsconfig.json
 
-IS_LOCAL_BUILD=false || exit 1
+IS_LOCAL_BUILD=true || exit 1
 if [ "$IS_LOCAL_BUILD" = true ]; then
   echo "Using blinkid-react-native from this repo instead from NPM"
   # Enter the BlinkID folder
@@ -37,7 +37,7 @@ if [ "$IS_LOCAL_BUILD" = true ]; then
 
   # Go the sample folder and install the library
   popd > /dev/null
-  npm i --save $blink_id_plugin_path/microblink-blinkid-react-native-7.6.1.tgz
+  npm i --save $blink_id_plugin_path/microblink-blinkid-react-native-7.6.2.tgz
 
 else
   # Download BlinkID React Native via NPM
@@ -74,7 +74,10 @@ sed -i '' '/<dict>/a\
 # Disable Flipper since it spams console with errors
 export NO_FLIPPER=1
 
+# RCT_NEW_ARCH_ENABLED=0 pod install
 pod install
+
+# pod install
 
 # Return from the ios project folder
 popd
